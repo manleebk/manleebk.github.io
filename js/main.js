659c8a82002d3b67fc6caaaddc4c641642c04454
manleebk <levanman1997@gmail.com>
2020-08-05 10:35:35 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -98,7 +98,7 @@ function start() {
   const videoSource = videoSelect.value;
   const constraints = {
     audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
-    video: {deviceId: undefined ? {exact: undefined} : undefined}
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
   };
   navigator.mediaDevices.getUserMedia(constraints).then(undefined).then(gotDevices).catch(handleError);
 }
